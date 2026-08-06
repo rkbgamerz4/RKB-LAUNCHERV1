72c66764404b5c4ba93700ebfbaf4b01dc77423a
rkbgamerz4 <rkbgamerz4@gmail.com>
2026-08-06 10:13:19 +0600

    Update NativeThemeUtils.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/theme/NativeThemeUtils.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/theme/NativeThemeUtils.kt
@@ -1,4 +1,5 @@
-Zalith Launcher 2
+ /*
+ *Zalith Launcher 2
  * Copyright (C) 2025 MovTery <movtery228@qq.com> and contributors
  *
  * This program is free software: you can redistribute it and/or modify
