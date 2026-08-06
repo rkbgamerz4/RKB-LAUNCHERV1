ed4fdd643847552ec0a0d9ef9a3487517e8b2f59
rkbgamerz4 <rkbgamerz4@gmail.com>
2026-08-06 10:26:34 +0600

    Update Palette.kt

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/theme/Palette.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/theme/Palette.kt
@@ -15,7 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.txt>.
  */
-
 package com.movtery.zalithlauncher.ui.theme
 
 import androidx.compose.material3.MaterialTheme
